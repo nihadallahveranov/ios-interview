bd08f4713e7c48a470ed3e84b9b9790dba01dced
Nihad Allahveranov <nihad.allahveranov@gmail.com>
2024-01-17 10:40:06 +0400

    initial

---
+++ Pods/Target Support Files/Pods-iOS-Interview-iOS-InterviewUITests/Pods-iOS-Interview-iOS-InterviewUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS_Interview_iOS_InterviewUITests : NSObject
+@end
+@implementation PodsDummy_Pods_iOS_Interview_iOS_InterviewUITests
+@end
---
+++ iOS-Interview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Interview
+//
+//  Created by Nihad Allahveranov on 21.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
